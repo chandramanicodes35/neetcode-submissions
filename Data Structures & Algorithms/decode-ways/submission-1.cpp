b572f4973e11e460df74c811195e7e7eb19f4245
Chandramani Yadav <148085070+chandramanicodes35@users.noreply.github.com>
2026-07-20 14:22:26 +0530

    Add: decode-ways - submission-1

---
+++ Data Structures & Algorithms/decode-ways/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int numDecodings(string s) {
+       if(s.empty()|| s[0]=='0')
+       return 0;
+       int prev2=1;
+       int prev1=1;
+       int curr = 0;
+       for(int i=1;i<s.size();i++){
+        if(s[i]!='0'){
+            curr = prev1;
+
+            int twodigit = stoi(s.substr(i-1,2));
+            int (twodigit >= 10 && twodigit <= 26);
+                curr += prev2;
+        }
+
+        prev2=prev1;
+        prev1=curr;
+
+       } 
+       return prev1;
+    }
+};
